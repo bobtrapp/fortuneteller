2c671abfd6aa717bf10dab7451dc28447245bdb2
Bob Trapp <bobgtrapp@gmail.com>
2022-04-19 11:16:30 -0500

    Build basic site structure

---
+++ js/app.js
@@ -0,0 +1,16 @@
+/**
+ * @file app.js
+ * @date 2022-04-19
+ * @author Bob Trapp
+ * @description Handles the fortune teller logic for the application
+ */
+"use strict";
+
+/**
+ * The initializeApp() function acts as a starting point for the entire
+ * application.  All other code should be reached through a path starting
+ * here or in response to an event.
+ */
+function initializeApp() {
+  console.log("Initialization function loaded.");
+}
